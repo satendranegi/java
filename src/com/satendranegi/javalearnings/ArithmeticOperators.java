431bfb7d317953038f94f4dd1391f4e6fd3a9aa2
satendra <satendranegi1989@gmail.com>
2021-08-27 20:01:27 +0530

    Adding arithmetic operators

---
+++ src/com/satendranegi/javalearnings/ArithmeticOperators.java
@@ -0,0 +1,23 @@
+package com.satendranegi.javalearnings;
+
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 6;
+
+        int r1 = a + b; //16
+        int r2 = a -b ; //4
+        int r3 = a * b; //60
+        int r4 = a / b; // 1
+        int remainder = a % b; //4
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+        System.out.println(remainder);
+
+    }
+}
